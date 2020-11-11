5cf698b1c1bb06651e0a6b997ed4938b11d84f6c
marco-nicola <marconicola@disroot.org>
2020-11-11 19:29:06 +0100

    Add NLP tokenizers normalizers and normalizedstring packages
    from gotokenizers project

---
+++ pkg/nlp/tokenizers/normalizers/normalizers.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2020, NLP Odyssey Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package normalizers
+
+import "github.com/nlpodyssey/spago/pkg/nlp/tokenizers/normalizers/normalizedstring"
+
+// Normalizer is implemented by any value that has a Normalize method,
+// which takes care of pre-processing strings.
+type Normalizer interface {
+	Normalize(ns *normalizedstring.NormalizedString) error
+}
